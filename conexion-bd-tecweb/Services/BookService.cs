cbbffbc2519f8e65686f10aafe7f49e223c3d8a7
Alejandro Castro <alejandro.castro.0520@gmail.com>
2025-10-05 16:34:32 -0400

    fix: Changed wrong files

---
+++ conexion-bd-tecweb/Services/BookService.cs
@@ -0,0 +1,49 @@
+﻿using apiwithdb.Models;
+using apiwithdb.Models.dtos;
+using apiwithdb.Repositories;
+
+namespace apiwithdb.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly IBookRepository _repo;
+        public BookService(IBookRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<Book> Create(CreateBookDto dto)
+        {
+            if(dto.Year<1900)
+            {
+                throw new InvalidOperationException("Year must be between 1900 and the current year.");
+            }
+            var book = new Book
+            {
+                Id = Guid.NewGuid(),
+                Title = dto.Title.Trim(),
+                Year = dto.Year
+            };
+            await _repo.Add(book);
+            return book;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            var existing = _repo.GetById(id);
+            if (existing == null) return false;
+            await _repo.Delete(id);
+            return true;
+        }
+
+        public async Task<IEnumerable<Book>> GetAll()
+        {
+            return await _repo.GetAll();
+        }
+
+        public async Task<Book?> GetById(Guid id)
+        {
+            var book = _repo.GetById(id);
+            return await book;
+        }
+    }
+}
