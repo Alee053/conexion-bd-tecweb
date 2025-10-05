194e14c1a5e0868c2408d632d098a02d233f6b94
Alejandro Castro <alejandro.castro.0520@gmail.com>
2025-10-05 16:33:47 -0400

    feat: Implemented TicketService and Interfacce

---
+++ conexion-bd-tecweb/Services/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using apiwithdb.Models;
+using apiwithdb.Models.dtos;
+
+namespace apiwithdb.Services
+{
+    public interface IBookService
+    {
+        Task<IEnumerable<Book>> GetAll();
+        Task<Book?> GetById(Guid id);
+        Task<Book> Create(CreateBookDto dto);
+        Task<bool> Delete(Guid id);
+    }
+}
