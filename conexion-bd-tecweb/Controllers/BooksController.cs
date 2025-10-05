8a347c4c32cfe0df783d9fe9f2df90298cc1100b
Alejandro Castro <alejandro.castro.0520@gmail.com>
2025-10-05 13:39:53 -0400

    feat: Initial commit, guide files

---
+++ conexion-bd-tecweb/Controllers/BooksController.cs
@@ -0,0 +1,49 @@
+﻿using apiwithdb.Models.dtos;
+using apiwithdb.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiwithdb.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController: ControllerBase
+    {
+        private readonly IBookService _service;
+        public BooksController(IBookService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _service.GetAll());
+        }
+
+        [HttpGet("{id:guid}")]
+        public async  Task<IActionResult> GetOne(Guid id)
+        {
+            var book = _service.GetById(id);
+            return await book == null
+                ? NotFound(new { error = "Book not found", status = 404 })
+                : Ok(book);
+        }
+
+        [HttpPost]
+        public async  Task<IActionResult> Create([FromBody] CreateBookDto dto)
+        {
+            if (!ModelState.IsValid) return ValidationProblem(ModelState);
+            var book = await _service.Create(dto);
+            return CreatedAtAction(nameof(GetOne), new { id = book.Id }, book);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var success = _service.Delete(id);
+            return await success
+                ? NoContent()
+                : NotFound(new { error = "Book not found", status = 404 });
+        }
+    }
+}
---
+++ conexion-bd-tecweb/Repositories/IBookRepository.cs
@@ -0,0 +1,12 @@
+﻿using apiwithdb.Models;
+
+namespace apiwithdb.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> GetAll();
+        Task<Book?> GetById(Guid id);
+        Task Add(Book book);
+        Task Delete(Guid id);
+    }
+}
