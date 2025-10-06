ce6d6dbde41ac3a65daf0ae91fcd49db103c718c
shalure10lure <yessicarado@gmail.com>
2025-10-05 21:54:18 -0400

    feat:adding GuestService

---
+++ conexion-bd-tecweb/Services/GuestService.cs
@@ -0,0 +1,15 @@
+﻿using conexion_bd_tecweb.Models;
+using conexion_bd_tecweb.Models.dtos;
+using conexion_bd_tecweb.Repositories;
+
+namespace conexion_bd_tecweb.Services
+{
+    public class GuestService: IGuestService
+    {
+        private readonly IGuestRepository _repo;
+        public GuestService(IGuestRepository repo)
+        {
+            _repo = repo;
+        }
+    }
+}
