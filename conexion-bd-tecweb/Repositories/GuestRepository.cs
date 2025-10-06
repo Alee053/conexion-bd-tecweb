de33d3cbdff9fe5020978580e26673c529238550
shalure10lure <yessicarado@gmail.com>
2025-10-05 21:34:53 -0400

    feat:adding function Add to Guest Repository

--- conexion-bd-tecweb/Repositories/GuestRepository.cs
+++ conexion-bd-tecweb/Repositories/GuestRepository.cs
@@ -1,15 +1,21 @@
-﻿using conexion_bd_tecweb.Data;
+﻿using apiwithdb.Models;
+using conexion_bd_tecweb.Data;
 using conexion_bd_tecweb.Models;
 using Microsoft.EntityFrameworkCore;
 namespace conexion_bd_tecweb.Repositories
 {
-    public class GuestRepository
+    public class GuestRepository: IGuestRepository
     {
         private readonly AppDbContext _context;
 
         public GuestRepository(AppDbContext context)
         {
             _context = context;
+        }
+        public async Task Add(Guest guest)
+        {
+            await _context.AddAsync(guest);
+            await _context.SaveChangesAsync();
         }
     }
 }
