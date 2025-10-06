a91ae204b36a7dc74c190b29db5b2d9dd34e9f07
shalure10lure <yessicarado@gmail.com>
2025-10-05 21:21:48 -0400

    feat:adding GuetsController

---
+++ conexion-bd-tecweb/Controllers/GuestsController.cs
@@ -0,0 +1,17 @@
+﻿using apiwithdb.Models.dtos;
+using apiwithdb.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace conexion_bd_tecweb.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GuestsController : ControllerBase
+    {
+        private readonly IGuestService _service;
+        public GuestsController(IGuestService service)
+        {
+            _service = service;
+        }
+    }
+}
