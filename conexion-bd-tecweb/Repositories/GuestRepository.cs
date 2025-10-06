9b6c295168297128a99a6a16b0b53baa9b5401e1
shalure10lure <yessicarado@gmail.com>
2025-10-05 21:31:38 -0400

    feat:adding Guest Repository

---
+++ conexion-bd-tecweb/Repositories/GuestRepository.cs
@@ -0,0 +1,15 @@
+﻿using conexion_bd_tecweb.Data;
+using conexion_bd_tecweb.Models;
+using Microsoft.EntityFrameworkCore;
+namespace conexion_bd_tecweb.Repositories
+{
+    public class GuestRepository
+    {
+        private readonly AppDbContext _context;
+
+        public GuestRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
