4fbd2d5ecf52f7731032659afdf9049c79dadb1b
shalure10lure <yessicarado@gmail.com>
2025-10-05 21:48:40 -0400

    feat:adding IGuestRepository

---
+++ conexion-bd-tecweb/Repositories/IGuestRepository.cs
@@ -0,0 +1,13 @@
+﻿using apiwithdb.Models;
+using conexion_bd_tecweb.Models;
+
+namespace conexion_bd_tecweb.Repositories
+{
+    public interface IGuestRepository
+    {
+        Task<IEnumerable<Guest>> GetAll();
+        Task<Guest?> GetById(Guid id);
+        Task Add(Guest guest);
+        Task Delete(Guid id);
+    }
+}
