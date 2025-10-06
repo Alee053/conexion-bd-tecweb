f63ea70478b1f4a056a7b6f85d10f1d65f7aeeac
shalure10lure <yessicarado@gmail.com>
2025-10-05 21:51:41 -0400

    feat:adding IGuestService

---
+++ conexion-bd-tecweb/Services/IGuestService.cs
@@ -0,0 +1,13 @@
+﻿using conexion_bd_tecweb.Models;
+using conexion_bd_tecweb.Models.dtos;
+
+namespace conexion_bd_tecweb.Services
+{
+    public interface IGuestService
+    {
+        Task<IEnumerable<Guest>> GetAll();
+        Task<Guest?> GetById(Guid id);
+        Task<Guest> Create(CreateGuestDto dto);
+        Task<bool> Delete(Guid id);
+    }
+}
