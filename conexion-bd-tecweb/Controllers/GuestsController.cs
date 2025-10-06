7cfc9655af29053914f9ec11bec28f528bdae249
shalure10lure <yessicarado@gmail.com>
2025-10-05 21:24:08 -0400

    feat:adding method GET with id to controller

--- conexion-bd-tecweb/Controllers/GuestsController.cs
+++ conexion-bd-tecweb/Controllers/GuestsController.cs
@@ -18,5 +18,14 @@ namespace conexion_bd_tecweb.Controllers
         {
             return Ok(await _service.GetAll());
         }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetOne(Guid id)
+        {
+            var guest = _service.GetById(id);
+            return await guest == null
+                ? NotFound(new { error = "Guest not found", status = 404 })
+                : Ok(guest);
+        }
     }
 }
