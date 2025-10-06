f8a08331c5e4158fc4ab2108940e2f5c8b38db28
PelucheW <clonesomg@gmail.com>
2025-10-05 20:25:43 -0400

    feat: create files dtos and adding CreateEventDto

--- conexion-bd-tecweb/Models/dtos/CreateEventDto.cs
+++ conexion-bd-tecweb/Models/dtos/CreateEventDto.cs
@@ -3,6 +3,14 @@ namespace conexion_bd_tecweb.Models.dtos
 {
     public class CreateEventDto
     {
-        
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public int Capacity { get; set; }
     }
 }
