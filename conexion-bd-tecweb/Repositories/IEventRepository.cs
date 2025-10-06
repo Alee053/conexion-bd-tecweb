04e8f9b9cfd505179f9e960f76dd2ee8554d5799
PelucheW <clonesomg@gmail.com>
2025-10-05 20:24:01 -0400

    feat: adding files Repositories, IEventRepository and EventRepository

--- conexion-bd-tecweb/Repositories/IEventRepository.cs
+++ conexion-bd-tecweb/Repositories/IEventRepository.cs
@@ -3,5 +3,11 @@ using conexion_bd_tecweb.Models;
 
 namespace apiwithdb.Repositories
 {
-    
+    public interface IEventRepository
+    {
+        IEnumerable<Event> GetAll();
+        Event? GetById(Guid id);
+        void Add(Event ev);
+        void Delete(Guid id);
+    }
 }
