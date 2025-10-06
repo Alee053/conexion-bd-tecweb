c00804d0d34849c4ded4b94f7b18fc8f77b940f1
shalure10lure <yessicarado@gmail.com>
2025-10-05 21:57:34 -0400

    feat:adding Create method to GuestService

--- conexion-bd-tecweb/Services/GuestService.cs
+++ conexion-bd-tecweb/Services/GuestService.cs
@@ -1,4 +1,6 @@
-﻿using conexion_bd_tecweb.Models;
+﻿using apiwithdb.Models;
+using apiwithdb.Models.dtos;
+using conexion_bd_tecweb.Models;
 using conexion_bd_tecweb.Models.dtos;
 using conexion_bd_tecweb.Repositories;
 
@@ -10,6 +12,17 @@ namespace conexion_bd_tecweb.Services
         public GuestService(IGuestRepository repo)
         {
             _repo = repo;
+        }
+        public async Task<Guest> Create(CreateGuestDto dto)
+        {
+            var guest = new Guest
+            {
+                Id = Guid.NewGuid(),
+                FullName = dto.FullName.Trim(),
+                Confirmed = dto.Confirmed
+            };
+            await _repo.Add(guest);
+            return guest;
         }
     }
 }
